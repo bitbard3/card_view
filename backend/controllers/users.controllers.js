428b98c36b5595811647abbcecbdb19a9b7d1784
bitbard3 <ansharora3839@gmail.com>
2024-04-02 20:11:38 +0530

    Add functionality to delete user

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -14,6 +14,7 @@ export const createUser = async (req, res) => {
             await User.create(payload)
             return res.json({ msg: "User created" })
         } catch (error) {
+            console.log(error)
             return res.status(500).json({ msg: "Internal server error" })
         }
     }
@@ -34,6 +35,21 @@ export const updateUser = async (req, res) => {
         try {
             await User.updateOne({ _id: userId }, payload)
             return res.json({ msg: "User updated successfully" })
+        } catch (error) {
+            return res.status(500).json({ msg: "Internal server error" })
+        }
+    } else {
+        return res.status(404).json({ msg: "User doesnt exist" })
+    }
+}
+
+export const deleteUser = async (req, res) => {
+    const userId = req.params.id
+    const userExist = await User.findOne({ _id: userId })
+    if (userExist) {
+        try {
+            await User.deleteOne({ _id: userId })
+            return res.json({ msg: "User deleted successfully" })
         } catch (error) {
             return res.status(500).json({ msg: "Internal server error" })
         }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,8 +1,9 @@
 import express from 'express'
-import { createUser, updateUser } from '../controllers/users.controllers.js'
+import { createUser, deleteUser, updateUser } from '../controllers/users.controllers.js'
 const router = express.Router()
 
 router.post('/', createUser)
 router.put('/:id', updateUser)
+router.delete('/:id', deleteUser)
 
 export default router
