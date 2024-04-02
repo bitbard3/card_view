938946483cff9d8d350f78ec6c925547f307e1c2
bitbard3 <ansharora3839@gmail.com>
2024-04-02 19:12:56 +0530

    Add update user functionality

--- backend/controllers/users.controllers.js
+++ backend/controllers/users.controllers.js
@@ -1,23 +1,43 @@
 import { User } from "../db/db.js"
-import { createUserSchmea } from "../validations/user.validation.js"
+import { createUserSchema, updateUserSchema } from "../validations/user.validation.js"
 
 export const createUser = async (req, res) => {
     const payload = req.body
-    const validPayload = createUserSchmea.safeParse(payload)
+    const validPayload = createUserSchema.safeParse(payload)
     if (validPayload.error) {
-        res.status(411).json({ msg: "Invalid payload" })
-        return
+        return res.status(411).json({ msg: "Invalid payload" })
+
     }
     const userExist = await User.findOne({ email: payload.email })
     if (!userExist) {
         try {
             await User.create(payload)
-            res.json({ msg: "User created" })
+            return res.json({ msg: "User created" })
         } catch (error) {
-            res.status(500).json({ msg: "Internal server error" })
+            return res.status(500).json({ msg: "Internal server error" })
         }
     }
     else {
-        res.status(409).json({ msg: "User already exist" })
+        return res.status(409).json({ msg: "User already exist" })
+    }
+}
+
+export const updateUser = async (req, res) => {
+    const userId = req.params.id
+    const payload = req.body
+    const validPayload = updateUserSchema.safeParse(payload).data
+    if (Object.keys(validPayload).length === 0) {
+        return res.status(411).json({ msg: "Invalid payload" })
+    }
+    const userExist = await User.findOne({ _id: userId })
+    if (userExist) {
+        try {
+            await User.updateOne({ _id: userId }, payload)
+            return res.json({ msg: "User updated successfully" })
+        } catch (error) {
+            return res.status(500).json({ msg: "Internal server error" })
+        }
+    } else {
+        return res.status(404).json({ msg: "User doesnt exist" })
     }
 }
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,7 +1,8 @@
 import express from 'express'
-import { createUser } from '../controllers/users.controllers.js'
+import { createUser, updateUser } from '../controllers/users.controllers.js'
 const router = express.Router()
 
 router.post('/', createUser)
+router.put('/:id', updateUser)
 
 export default router
