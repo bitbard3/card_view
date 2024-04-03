9e4899e60f23a416fc231e1ac872878b75fd32ec
bitbard3 <ansharora3839@gmail.com>
2024-04-04 03:27:57 +0530

    Add action component

---
+++ frontend/src/components/Actions.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { LuListFilter } from "react-icons/lu";
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from "@/components/ui/popover";
+import FilterPopover from './FilterPopover';
+export default function Actions({ setDomains, domains }) {
+    return (
+        <div className="flex items-center space-x-10 ml-auto">
+            <div className="h-10 w-10 rounded-full hover:opacity-95 bg-[#788fff] flex items-center justify-center">
+                <Popover>
+                    <PopoverTrigger>
+                        <div className="">
+                            <LuListFilter className='text-white h-6 w-6' />
+                        </div>
+                    </PopoverTrigger>
+                    <PopoverContent>
+                        <FilterPopover domains={domains} setDomains={setDomains} />
+                    </PopoverContent>
+                </Popover>
+            </div>
+            <div className="text-white bg-[#788fff] rounded-lg hover:opacity-95 px-10 py-1.5 text-lg">
+                Add team
+            </div>
+        </div>
+    )
+}
