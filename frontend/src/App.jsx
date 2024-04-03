deb01d9239ccb97aa1d31022a40c3f35e0e2d55c
bitbard3 <ansharora3839@gmail.com>
2024-04-03 09:06:43 +0530

    Add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import List from "./pages/List";
 
 function App() {
 
 
   return (
     <>
-
+      <Routes>
+        <Route path="/" element={<List />} />
+      </Routes>
     </>
   )
 }
