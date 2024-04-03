644d0caec06ba0bbb966ba83fd85c64d99cd3ba9
bitbard3 <ansharora3839@gmail.com>
2024-04-04 03:27:37 +0530

    Add popover element

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -4,15 +4,18 @@ import { NavLink } from "react-router-dom";
 export default function NavBar() {
     return (
         <>
-            <NavLink to={'/'} className={({ isActive }) =>
-                isActive ? 'text-white text-xl border-b-[1px] border-neutral-300' : 'text-neutral-400 text-xl'
-            }>
-                Users
-            </NavLink>
-            <NavLink to={'/teams'} className={({ isActive }) =>
-                isActive ? 'text-white text-xl' : 'text-neutral-400 text-xl'
-            }>
-                Teams
-            </NavLink></>
+            <div className="flex items-center space-x-8">
+                <NavLink to={'/'} className={({ isActive }) =>
+                    isActive ? 'text-white text-xl border-b-[1px] border-neutral-300' : 'text-neutral-400 text-xl'
+                }>
+                    Users
+                </NavLink>
+                <NavLink to={'/teams'} className={({ isActive }) =>
+                    isActive ? 'text-white text-xl' : 'text-neutral-400 text-xl'
+                }>
+                    Teams
+                </NavLink>
+            </div>
+        </>
     )
 }
--- frontend/src/pages/List.jsx
+++ frontend/src/pages/List.jsx
@@ -10,29 +10,31 @@ import {
     PaginationNext,
     PaginationPrevious,
 } from "@/components/ui/pagination"
+import Actions from '@/components/Actions';
 
 export default function List() {
     const { search } = useLocation();
     const queryParams = new URLSearchParams(search);
     const [currentPage, setCurrentPage] = useState(parseInt(queryParams.get('page')) || 1);
     const [users, setUsers] = useState([]);
-    const [loading, setLoading] = useState(true)
-
+    const [loading, setLoading] = useState(true);
+    const [domains, setDomains] = useState(queryParams.get('domain') ? queryParams.get('domain').split(',') : []);
+    const [genders, set] = useState(queryParams.get('domain') ? queryParams.get('domain').split(',') : []);
 
     useEffect(() => {
         setLoading(true);
         const fetchData = async (page) => {
             try {
-                const res = await axios.get(`https://card-view-backend.vercel.app/api/users?page=${page}`);
+                const domainQuery = domains.join(',');
+                const res = await axios.get(`https://card-view-backend.vercel.app/api/users?page=${page}&domain=${domainQuery}`);
                 setUsers(res.data.users);
             } catch (error) {
-
             } finally {
                 setLoading(false);
             }
         };
         fetchData(currentPage);
-    }, [currentPage]);
+    }, [currentPage, domains]);
 
     const handlePreviousPage = () => {
         if (currentPage > 1) {
@@ -47,8 +49,12 @@ export default function List() {
     return (
         <div className="bg-black min-h-screen w-screen">
             <div className="flex flex-col w-full h-full pb-10">
-                <div className="w-full flex justify-center pb-3 space-x-16 items-end h-28 border-b-2 border-neutral-600">
+                <div className="w-full flex  px-20 pb-3 items-end h-28 border-b-2 border-neutral-600">
                     <NavBar />
+                    <Actions
+                        domains={domains}
+                        setDomains={setDomains}
+                    />
                 </div>
                 <div className="w-full py-10 gap-y-10 gap-x-5 grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4  grid-cols-1 px-10 ">
                     <CardList loading={loading} users={users} />
